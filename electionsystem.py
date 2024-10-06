b242a09a354207a1a35f54ce261abd6eaa9d4382
Vishal Balasubramanian <vishal.bala.nps@gmail.com>
2024-10-06 08:40:43 +0530

    Displays votes

--- electionsystem.py
+++ electionsystem.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import getpass
+from tabulate import tabulate
 
 # Server Details
 host = "localhost"
@@ -19,6 +20,16 @@ def init_tables():
     except mysql.connector.Error as e:
         print("An error occurred! Error:",e)
         return -1
+def checkTie(lst):
+    ifirst = lst[0]
+    tievals = [ifirst]
+    for i in range(1,len(lst)):
+        if lst[i][4] == ifirst[4]:
+            ifirst = lst[i]
+            tievals.append(lst[i])
+        else:
+            break
+    return tievals
 username =  input("Please enter username: ")
 password =  getpass.getpass('Please enter password: ') #Used to get a password from user (so that password is not shown)
 
@@ -74,7 +85,7 @@ try:
                     cursor.execute("select * from candidates where election={0} order by id".format(op))
                     cf = cursor.fetchall()
                     if len(cf) == 0:
-                        print("No candidates added or invalid election id")
+                        print("Empty election or invalid election id")
                     else:
                         print("Please select candidate:")
                         for i in cf:
@@ -193,9 +204,40 @@ try:
                 else:
                     print("Invalid option!")
         elif op == 5 and superuser:
-            print("view result")
+            try:
+                cursor.execute("select * from elections order by id")
+            except Exception as e:
+                print("Failed to get list of elections! Error:",e)
+                continue
+            elections = cursor.fetchall()
+            print()
+            while True:
+                print("Please select election: ")
+                for i in elections:
+                    print(i[0],". ",i[1],sep="")
+                op = input("Enter election id (press enter to go back): ")
+                if op == "":
+                    break
+                else:
+                    cursor.execute("select id,name,class,section,votes from candidates,votes where candidates.id=votes.candidate and votes.election={0} order by votes.votes desc".format(op))
+                    cf = cursor.fetchall()
+                    if len(cf) == 0:
+                        print("Empty election or invalid election id")
+                    else:
+                        print(tabulate(cf,["Candidate ID","Name","Class","Section","No of votes"]))
+                    tie = checkTie(cf)
+                    if len(tie) == 1:
+                        print("Candidate with most votes:")
+                        print(tie[0][1]+" of class ",tie[0][2],tie[0][3]," with ",tie[0][4]," votes",sep="")
+                    else:
+                        print("Results tied between:")
+                        for i in range(len(tie)):
+                            print(i+1,". ",tie[i][1],sep="")
+                        print("These candidates have ",tie[0][4]," votes",sep="")
         elif op == exitval:
             print("Exiting")
+            cursor.close()
+            con.close()
             exit()
         else:
             print("Invalid option!")
